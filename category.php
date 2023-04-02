faaf8d20efa680641defb780170c323accbf2a4d
Francesco Punta <puntafrancesco@gmail.com>
2023-04-02 15:55:44 +0200

    Tolgo product da category

--- category.php
+++ category.php
@@ -2,22 +2,13 @@
 class Category {
     private $id;
     private $name;
-    private $products;
 
     public function __construct($id, $name) {
         $this->id = $id;
         $this->name = $name;
-        $this->products = [];
-    }
-
-    public function addProduct($product) {
-        $this->products[] = $product;
-    }
-
-    public function getProducts() {
-        return $this->products;
     }
 
 }
+
 
 ?>
