348142e5ad8bf4a959e47cf1cebb2852edb7dfc4
Francesco Punta <puntafrancesco@gmail.com>
2023-04-02 16:37:51 +0200

    Category

--- category.php
+++ category.php
@@ -1,14 +1,26 @@
 <?php
 class Category {
-    private $id;
-    private $name;
+	private $id;
+	private $nome;
+	private $icona;
 
-    public function __construct($id, $name) {
-        $this->id = $id;
-        $this->name = $name;
-    }
+	public function __construct($id, $nome, $icona) {
+		$this->id = $id;
+		$this->nome = $nome;
+		$this->icona = $icona;
+	}
 
+	public function getId() {
+		return $this->id;
+	}
+
+	public function getNome() {
+		return $this->nome;
+	}
+
+	public function getIcona() {
+		return $this->icona;
+	}
 }
-
 
 ?>
