66f92f61dd793cd3d75382373ef6d70afd41831e
Francesco Punta <puntafrancesco@gmail.com>
2023-04-03 17:13:07 +0200

    Risoluzione problema visualizzazione card e test trait

--- index.php
+++ index.php
@@ -15,8 +15,7 @@ $cibo_cane = new Food(3, 'Cibo per cane', 'cibo per cani', 19.99, 'dog_food.jpg'
 $cibo_gatto = new Food(4, 'Cibo per gatto', 'cibo per gatti', 15.99, 'cat_food.jpg', $categoria_gatti);
 
 
-$prodotti = [$giocattolo_cane, $giocattolo_gatto, $cibo_gatto, $cibo_cane];
-var_dump($prodotti)
+$prodotti = [$giocattolo_cane, $giocattolo_gatto,$cibo_cane, $cibo_gatto ];
 ?>
 
 <!DOCTYPE html>
@@ -37,24 +36,16 @@ var_dump($prodotti)
 
     <main>
         <div class="products">
-            <?php echo count($prodotti); var_dump($giocattolo_cane); var_dump($giocattolo_gatto); var_dump($cibo_cane); var_dump($cibo_gatto); ?> 
             <?php for ($i=0; $i<count($prodotti); $i++){ ?>
                 <?php $prodotto = $prodotti[$i]; ?>
-
-                <pre><?php echo $i; echo $prodotto->getId(); ?></pre>
-
-                <br>
-                <br>
-                <br>
-
-<!--                  <div class="product-card">
+                     <div class="product-card">
                     <img src="<?php echo $prodotto->getImmagine(); ?>" alt="<?php echo $prodotto->getNome(); ?>">
                     <h2><?php echo $prodotto->getNome(); ?></h2>
                     <p><?php echo $prodotto->getDescrizione(); ?></p>
                     <p><?php echo $prodotto->getPrezzo(); ?> €</p>
                     <span><?php echo $prodotto->getCategoria()->getIcona() . ' ' . $prodotto->getCategoria()->getNome(); ?></span>
-                    <span><?php echo $prodotto->isToy() ? 'Giocattolo' : ($prodotto->isFood() ? 'Cibo' : 'Cuccia'); ?></span>
-                </div> -->
+                    <span><?php echo get_class($prodotto) === 'Toy' ? 'Giocattolo' : (get_class($prodotto) === 'Food' ? 'Cibo' : 'Cuccia'); ?></span>
+                </div>
             <?php } ?>
         </div>
     </main>
