bd61a6f94e851bfe6d769dc88c90a762de7d284e
Francesco Punta <puntafrancesco@gmail.com>
2023-04-03 17:23:32 +0200

    Trait expiration

--- food.php
+++ food.php
@@ -1,9 +1,25 @@
 <?php
-require_once 'product.php';
+require_once 'Product.php';
 
 class Food extends Product {
-    public function isFood() {
-        return true;
+    private $scadenza;
+
+    public function __construct($id, $nome, $descrizione, $prezzo, $immagine, $categoria, $scadenza) {
+        parent::__construct($id, $nome, $descrizione, $prezzo, $immagine, $categoria);
+        $this->scadenza = $scadenza;
+    }
+
+    public function getScadenza() {
+        return $this->scadenza;
     }
 }
+
+trait Expiration {
+    public function isExpired() {
+        $now = new DateTime();
+        return $this->getScadenza() < $now;
+    }
+}
+
 ?>
+
